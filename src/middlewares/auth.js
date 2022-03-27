5c6396d522e2b1b06a5dcf4ab6146c58d4b009fc
Nguyenle23___ <beenbin2308@gmail.com>
2022-03-27 22:28:07 +0700

    Create 3 middlewares
    - Auth user with token (accessToken, refeshToken)
    - Authorize for checking if user login or not before access to route
    - passport-authen replace Auth to check if user login normal or via FB

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
 
 const verifyToken = (req, res, next) => {
     // const authHeader = req.header('Authorization');
