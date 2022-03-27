21a276522bcc8e0a9616b2c75bda6495b2a2aac4
Nguyenle23___ <beenbin2308@gmail.com>
2022-03-27 22:31:16 +0700

    Update route product for test authen middleware

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../middlewares/authorize');
 
 const productController = require('../app/controllers/ProductController');
 
-router.get('/', productController.showProductList)
 router.get('/:gender', productController.filterGender)
-
 router.get('/:id', productController.showProductDetail);
+router.get('/', ensureAuthenticated, productController.showProductList)
+
 
 
 module.exports = router;
