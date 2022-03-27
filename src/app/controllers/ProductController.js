810c0f8883d6247d41e1bc39e6505e99e1b695bf
Nguyenle23___ <beenbin2308@gmail.com>
2022-03-27 22:35:09 +0700

    Update ProductController.js

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,46 +1,45 @@
-const Product = require('../models/Product')
-const Color = require('../models/Color')
-const Category = require('../models/Category')
-const { mongooseToObject } = require('../../config/utility/mongoose')
-const { multipleToObject } = require('../../config/utility/mongoose')
+const Product = require('../models/Product');
+const Color = require('../models/Color');
+const Category = require('../models/Category');
+
+const { mongooseToObject } = require('../../config/utility/mongoose');
+const { multipleToObject } = require('../../config/utility/mongoose');
 
 // [GET] /product
-const showProductList = async (req, res, next) => {
+const showProductList = async(req, res, next) => {
 
-  // Find product and product variation
+    // Find product and product variation
     let p = await Product.find();
     let colors = await Color.find();
     let categories = await Category.find();
 
-    res.render('TabProduct/product', {layout: 'mainClient.hbs', p: multipleToObject(p), color: multipleToObject(colors), category: multipleToObject(categories)});
+    res.render('TabProduct/product', { layout: 'mainClient.hbs', p: multipleToObject(p), color: multipleToObject(colors), category: multipleToObject(categories) });
 }
 
 // [GET] /product/category_id
-const filterGender = async (req, res, next) => {
+const filterGender = async(req, res, next) => {
 
-  let p = await Product.find({gender: req.params.gender})
+    let p = await Product.find({ gender: req.params.gender })
 
-  let categories = await Category.find();
-  let colors = await Color.find();
+    let categories = await Category.find();
+    let colors = await Color.find();
 
-  res.render('TabProduct/product', {layout: 'mainClient.hbs', p: multipleToObject(p), category: multipleToObject(categories), color: multipleToObject(colors) })
+    res.render('TabProduct/product', { layout: 'mainClient.hbs', p: multipleToObject(p), category: multipleToObject(categories), color: multipleToObject(colors) })
 }
 
 // [GET] /id
 const showProductDetail = (req, res, next) => {
-  Product.findOne({_id: req.params.id}).populate([
-    { path: 'cat_id' },
-    { path: 'image_id' },
-    // { path: 'image_id' },
-    { path: 'p_variations.pv_id', populate: [{ path: 'vari_color' }, { path: 'vari_size' }, { path: 'SKU' }, { path: 'vari_image' },] }
-  ])
-      .then(product =>        
-        res.render('productdetail', { product: mongooseToObject(product) })
-      )
-      .catch(next);
+    Product.findOne({ _id: req.params.id }).populate([
+            { path: 'cat_id' },
+            { path: 'image_id' },
+            // { path: 'image_id' },
+            { path: 'p_variations.pv_id', populate: [{ path: 'vari_color' }, { path: 'vari_size' }, { path: 'SKU' }, { path: 'vari_image' }, ] }
+        ])
+        .then(product =>
+            res.render('productdetail', { layout: 'mainClient.hbs', product: mongooseToObject(product) })
+        )
+        .catch(next);
 }
 
 
-module.exports = { showProductList, filterGender, showProductDetail}
-
-
+module.exports = { showProductList, filterGender, showProductDetail }
