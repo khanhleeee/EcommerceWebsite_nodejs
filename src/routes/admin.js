e8272cc5cbeccf254f47c79ef6fe9cfbf1d41bd9
Nguyenle23 <beenbin2308@gmail.com>
2022-03-28 23:20:10 +0700

    Update admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,6 @@ const verifyToken = require('../middlewares/auth');
 
 const adminController = require('../app/controllers/AdminController');
 
-router.delete('/:id', adminController.deleteProduct);
 router.get('/', adminController.showAdmin)
 
 module.exports = router;
