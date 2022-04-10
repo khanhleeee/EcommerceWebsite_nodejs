0bae3e5d448f613f3583a2868ace609065dc2630
Nguyenle23___ <beenbin2308@gmail.com>
2022-04-10 11:53:55 +0700

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated } = require('../middlewares/authorize');
 
 const productController = require('../app/controllers/ProductController');
 
 router.get('/:gender', productController.filterGender)
 router.get('/:id', productController.showProductDetail);
-router.get('/', ensureAuthenticated, productController.showProductList)
+router.get('/', productController.showProductList)
 
 
 
