a4c183d21d282770a6e3ac3d9f465c2f6f85e945
KhanhLeee <93957393+KhanhLeee@users.noreply.github.com>
2022-04-17 21:07:24 +0700

    sua lai QR momo

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -36,11 +36,11 @@ const payCOD = async(req, res, next) => {
         order.items.push({sku: req.body.sku[i], qty: req.body.qty[i], price: req.body.price[i]})
     }
    
-    res.json(order)
+    // res.json(order)
 
     try {
-        // await order.save();
-        // res.redirect('/payment/' + order.id + '/order');
+        await order.save();
+        res.redirect('/payment/' + order.id + '/order');
     } catch (err) {
         res.status(400).send(err);
     }
@@ -48,102 +48,92 @@ const payCOD = async(req, res, next) => {
 
 // [GET] /payment/:id/order
 const showOrder = async(req, res, next) => {
-    // const payment = await Payment.findById(req.params.id);
+    const payment = await Order.findById(req.params.id);
     // // console.log(payment);
-    // res.render('TabOrder/order', { layout: 'mainEmpty.hbs', payment: mongooseToObject(payment) });
+    res.render('TabOrder/order', { layout: 'mainEmpty.hbs', payment: mongooseToObject(payment) });
 }
 
 // [POST] /payment/:id/payOrder
 const payOrder = async(req, res, next) => {
-    //parameters
-    var partnerCode = "MOMOUW4Y20220414";
-    var accessKey = "PxadjEKn577xXABk";
-    var secretkey = "U0rXFOEyO6u1DFqpOxN7ua6G806NOqao";
-    var requestId = partnerCode + new Date().getTime();
-    var orderId = requestId;
-    var orderInfo = "pay with MoMo";
-    var redirectUrl = "http://localhost:3000/login";
-    var ipnUrl = "http://localhost:3000/payment";
-    // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
-    var amount = "5000";
-    var requestType = "captureWallet"
-    var extraData = ""; //pass empty value if your merchant does not have stores
+    //https://developers.momo.vn/#/docs/en/aiov2/?id=payment-method
+//parameters
+var partnerCode = "MOMOUW4Y20220414";
+var accessKey = "PxadjEKn577xXABk";
+var secretkey = "U0rXFOEyO6u1DFqpOxN7ua6G806NOqao";
+var requestId = partnerCode + new Date().getTime();
+var orderId = requestId;
+var orderInfo = "pay with MoMo";
+var redirectUrl = "https://momo.vn/return";
+var ipnUrl = "https://callback.url/notify";
+// var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
+var amount = "50000";
+var requestType = "captureWallet"
+var extraData = ""; //pass empty value if your merchant does not have stores
 
-    //before sign HMAC SHA256 with format
-    //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
-    var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType
-        //puts raw signature
-    console.log("--------------------RAW SIGNATURE----------------")
-    console.log(rawSignature)
-        //signature
-    const crypto = require('crypto');
-    var signature = crypto.createHmac('sha256', secretkey)
-        .update(rawSignature)
-        .digest('hex');
-    console.log("--------------------SIGNATURE----------------")
-    console.log(signature)
-        //json object send to MoMo endpoint
-    const requestBody = JSON.stringify({
-        partnerCode: partnerCode,
-        accessKey: accessKey,
-        requestId: requestId,
-        amount: amount,
-        orderId: orderId,
-        orderInfo: orderInfo,
-        redirectUrl: redirectUrl,
-        ipnUrl: ipnUrl,
-        extraData: extraData,
-        requestType: requestType,
-        signature: signature,
-        lang: 'en'
-    });
-    //Create the HTTPS objects
-    const https = require('https');
-    const options = {
-        hostname: 'test-payment.momo.vn',
-        port: 443,
-        path: '/v2/gateway/api/create',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(requestBody)
-        }
+//before sign HMAC SHA256 with format
+//accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+var rawSignature = "accessKey="+accessKey+"&amount=" + amount+"&extraData=" + extraData+"&ipnUrl=" + ipnUrl+"&orderId=" + orderId+"&orderInfo=" + orderInfo+"&partnerCode=" + partnerCode +"&redirectUrl=" + redirectUrl+"&requestId=" + requestId+"&requestType=" + requestType
+//puts raw signature
+console.log("--------------------RAW SIGNATURE----------------")
+console.log(rawSignature)
+//signature
+const crypto = require('crypto');
+var signature = crypto.createHmac('sha256', secretkey)
+    .update(rawSignature)
+    .digest('hex');
+console.log("--------------------SIGNATURE----------------")
+console.log(signature)
+
+//json object send to MoMo endpoint
+const requestBody = JSON.stringify({
+    partnerCode : partnerCode,
+    accessKey : accessKey,
+    requestId : requestId,
+    amount : amount,
+    orderId : orderId,
+    orderInfo : orderInfo,
+    redirectUrl : redirectUrl,
+    ipnUrl : ipnUrl,
+    extraData : extraData,
+    requestType : requestType,
+    signature : signature,
+    lang: 'en'
+});
+//Create the HTTPS objects
+const https = require('https');
+const options = {
+    hostname: 'test-payment.momo.vn',
+    port: 443,
+    path: '/v2/gateway/api/create',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(requestBody)
     }
+}
+//Send the request and get the response
+const req2 = https.request(options, res => {
+    console.log(`Status: ${res.statusCode}`);
+    console.log(`Headers: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (body) => {
+        console.log('Body: ');
+        console.log(body);
+        console.log('payUrl: ');
+        console.log(JSON.parse(body).payUrl);
+    });
+    res.on('end', () => {
+        console.log('No more data in response.');
+    });
+})
 
-    const req2 = () => {
-            return new Promise((resolve, reject) => {
-                //Send the request and get the response
-                let req1 = https.request(options, res => {
-                    // console.log(`Status: ${res.statusCode}`);
-                    // console.log(`Headers: ${JSON.stringify(res.headers)}`);
-                    res.setEncoding('utf8');
-                    let str;
-                    res.on('data', (body) => {
-                        console.log('Body: ');
-                        console.log('cc', body);
-                        console.log('payUrl: ');
-                        console.log(JSON.parse(body).payUrl);
-                        str = JSON.parse(body).payUrl;
-                        string = JSON.stringify(str);
-                        // console.log('string', string)
-                    });
-                    res.on('end', (body) => {
-                        console.log('No more data in response.');
-                    });
-                    // console.log(str);
-                });
-                req1.write(requestBody);
-                req1.end(
-                    res.redirect('/product')
-                );
-                // req1.on('error', (e) => {
-                //     console.log(`problem with request: ${e.message}`);
-                // });
-                // // write data to request body
-                // console.log("Sending....")
-            })
-        }
-        // req2().then((str) => { console.log('cc1', str); }).catch(console.log);
+req2.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+});
+// write data to request body
+console.log("Sending....")
+req2.write(requestBody);
+req2.end();
 }
 
 module.exports = { showPayment, payCOD, showOrder, payOrder }
